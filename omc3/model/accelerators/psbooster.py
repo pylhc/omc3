24b0fe407d09358a5056c6b094b9bd61868b17cd
PETRA Console User <petracon@bkrmcpecon1.desy.de>
2019-06-26 19:55:49 +0200

    fix parser for booster

--- omc3/model/accelerators/psbooster.py
+++ omc3/model/accelerators/psbooster.py
@@ -5,7 +5,7 @@ PS BOOSTER
 import os
 import re
 from model.accelerators.accelerator import Accelerator
-from parser.entrypoint import EntryPointParameters
+from omcparser.entrypoint import EntryPointParameters
 import logging
 
 LOGGER = logging.getLogger(__name__)
