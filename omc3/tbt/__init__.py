9f47839f245570d6eb5b9910a2809ddd46530f7d
mihofer <michael.hofer@cern.ch>
2019-07-30 18:22:39 +0200

    Enhancement/66/tbt loader (#74)
    * handler split up into class file and accel loader
    
    * changed hole in one to use diff file loader
    
    * remove handler as is replaced by data_class and accel_handler
    
    * add unit test for iota handler and refine read routine

--- omc3/tbt/__init__.py
+++ omc3/tbt/__init__.py
@@ -1,5 +1,2 @@
-"""Exposes TbtData, read_tbt and write_tbt directly in tbt namespace."""
-from tbt.handler import read_tbt, write_tbt, TbtData
-# aliases
-read = read_tbt
-write = write_tbt
+"""Exposes TbtData directly in tbt namespace."""
+from tbt.data_class import TbtData
