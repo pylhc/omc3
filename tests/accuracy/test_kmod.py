ae22641f059188db72bc672d0901c384a2a59cb4
mihofer <a>
2019-08-02 11:25:24 +0200

    partial test for kmod

--- tests/accuracy/test_kmod.py
+++ tests/accuracy/test_kmod.py
@@ -1,5 +1,18 @@
 import pytest
+import os
+
+CURRENT_DIR = os.path.dirname(__file__)
+
+def test_kmod(  ):
+    pass
 
 
-
-def test_kmod(  )
+@pytest.fixture()
+def _test_files():
+    ql1_file = os.path.join(CURRENT_DIR, os.pardir, "inputs", "kmod", "MQXA.1L1.tfs")
+    qr1_file = os.path.join(CURRENT_DIR, os.pardir, "inputs", "kmod", "MQXA.1R1.tfs")
+    try:
+        yield ql1_file, qr1_file
+    finally:
+        if os.path.isfile(test_file):
+            os.remove(test_file)
