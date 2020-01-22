3f112ce2e9fa3b14f03d035b83b0a80bcc1a9751
JoschD <joschua.dilly@gmail.com>
2020-01-22 17:41:09 +0100

    #138 working waterfall

--- omc3/plot_spectrum.py
+++ omc3/plot_spectrum.py
@@ -194,6 +194,9 @@ def get_params():
                          type=str,
                          default=DEFAULTS.waterfall_cmap,
                          help="Colormap to use for waterfall plot.")
+    params.add_parameter(name="waterfall_common_plane_colors",
+                         action="store_true",
+                         help="Same colorbar scale for both planes in waterfall plots.")
     params.add_parameter(name="show_plots",
                          action="store_true",
                          help='Flag to show plots')
@@ -272,7 +275,7 @@ class FigureContainer(object):
         self.tunes = {p: [] for p in PLANES}
         self.nattunes = {p: [] for p in PLANES}
         self.path = path
-        self.minmax = {p: (0, 1) for p in PLANES}
+        self.minmax = {p: (1, 0) for p in PLANES}
 
     def add_data(self, label: str, new_data: dict):
         self.data[label] = new_data
@@ -360,6 +363,9 @@ def _get_data_for_bpm(data: dict, bpm: str, rescale: bool) -> dict:
             data_series[plane][AMPS] = amps.loc[idxs_data]
             if rescale:
                 data_series[plane][AMPS] = _rescale_amp(data_series[plane][AMPS])
+
+            if any(data_series[plane][AMPS].isna()):
+                raise Exception("NAN FOUND")
     return data_series
 
 
@@ -524,7 +530,7 @@ def _create_waterfall_plot(figures: dict, opt: DotDict) -> None:
         LOG.debug(f'   Plotting Figure: {fig_id}.')
         fig_cont.fig.canvas.set_window_title(fig_id)
 
-        _plot_waterfall(fig_cont, opt.line_width, opt.cmap)
+        _plot_waterfall(fig_cont, opt.line_width, opt.cmap, opt.common_plane_colors)
         _plot_lines(fig_cont, opt.lines)
         _format_waterfall_axes(fig_cont, opt.limits, opt.ncol_legend)
         _output_plot(fig_cont)
@@ -533,21 +539,24 @@ def _create_waterfall_plot(figures: dict, opt: DotDict) -> None:
         plt.show()
 
 
-def _plot_waterfall(fig_cont, line_width, cmap):
-    norm = colors.LogNorm(*fig_cont.minmax)
+def _plot_waterfall(fig_cont, line_width, cmap, common_plane_colors):
     for idx_plane, plane in enumerate(PLANES):
         ax = fig_cont.axes[idx_plane]
+        norm = _get_waterfall_norm(fig_cont.minmax, plane, common_plane_colors)
         for idx_data, (label, data) in enumerate(fig_cont.data.items()):
             if data[plane] is None:
                 continue
-            freqs = data[FREQS]
-            amps = data[AMPS]
+            freqs = data[plane][FREQS]
+            amps = data[plane][AMPS]
 
             if line_width == "auto":
+                freqs = freqs.sort_values()
+                amps = amps.loc[freqs.index]
+
                 f_values = freqs.to_numpy().T
                 freqs_mesh = np.tile(np.array([*f_values, .5]), [2, 1])
                 y_mesh = np.tile([idx_data - 0.5, idx_data + 0.5], [len(freqs) + 1, 1]).T
-                ax.pcolormesh(freqs_mesh, y_mesh, amps.T, cmap=cmap, norm=norm, zorder=-3)
+                ax.pcolormesh(freqs_mesh, y_mesh, amps.to_frame().T, cmap=cmap, norm=norm, zorder=-3)
             else:
                 lc = ax.vlines(x=freqs, ymin=idx_data - .5, ymax=idx_data + .5,
                                linestyles='solid', cmap=cmap, norm=norm,
@@ -557,11 +566,20 @@ def _plot_waterfall(fig_cont, line_width, cmap):
         ax.figure.colorbar(matplotlib.cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax)
 
 
+def _get_waterfall_norm(minmax, plane, common):
+    """ Get color norm either from ylimits (if given) else from min and max values. """
+    if not common:
+        return colors.LogNorm(*minmax[plane])
+
+    return colors.LogNorm(min(mm[0] for mm in minmax.values()),
+                          max(mm[1] for mm in minmax.values()))
+
+
 # Finalize ---
 
 
 def _format_waterfall_axes(fig_cont, limits, ncol):
-    ylabels = fig_cont.labels
+    ylabels = fig_cont.data.keys()
     for idx_plane, plane in enumerate(PLANES):
         ax = fig_cont.axes[idx_plane]
         if ncol < 1:
@@ -722,7 +740,8 @@ def _sort_opt(opt):
     stem['plot'] = 'stem' in opt.plot_type
 
     # waterfall-plot options
-    waterfall = opt.get_subdict(('waterfall_line_width', 'waterfall_cmap', 'ncol_legend'))
+    waterfall = opt.get_subdict(('waterfall_line_width', 'waterfall_cmap', 'waterfall_common_plane_colors',
+                                 'ncol_legend'))
     waterfall = _rename_dict_keys(waterfall, to_remove="waterfall_")
     waterfall['plot'] = 'waterfall' in opt.plot_type
 
@@ -858,8 +877,8 @@ def _get_valid_indices(amps, freqs):
 
 
 def index_filter(data):
-    """ Only non-NaN and non-Zero data allowed. (Should not be zero due to _filter_amps() anyway.)"""
-    return data[~(data.isna() | data == 0)].index
+    """ Only non-NaN and non-Zero data allowed. (Amps should not be zero due to _filter_amps() anyway.)"""
+    return data[~(data.isna() | (data == 0))].index
 
 
 def _filter_amps(files, limit):
@@ -917,7 +936,7 @@ def list2str(list_):
 
 def _rescale_amp(amp_data):
     # return amp_data.divide(amp_data.max(axis=0), axis=1)  # dataframe
-    return amp_data.divide(amp_data.max())  # series
+    return amp_data.divide(amp_data.max(skipna=True))  # series
 
 
 def _get_all_bpms(bpms_dict):
