74a99faab9f012a1775ae079f29088b8d205ba74
awegsche <a.wegscheider7141@gmail.com>
2020-02-10 12:52:15 +0100

    cleanup

--- omc3/model/accelerators/petra.py
+++ omc3/model/accelerators/petra.py
@@ -5,12 +5,12 @@ PETRA
 import os
 import datetime as dt
 from omc3.model.accelerators.accelerator import Accelerator, AcceleratorDefinitionError
+from omc3.model.constants import PLANE_TO_HV
 import logging
 
 LOGGER = logging.getLogger(__name__)
 
 CURRENT_DIR = os.path.dirname(__file__)
-#PS_DIR = os.path.join(CURRENT_DIR, "ps")
 
 EXCITER_BPM = "BPM_SOR_13"
 
@@ -24,14 +24,6 @@ class Petra(Accelerator):
         if self.model_dir is None:  # is the class is used to create full response?
             raise AcceleratorDefinitionError("PETRA doesn't have a model creation yet, calling it this "
                                              "way is most probably wrong.")
-    def get_beam_direction(self):
-        return 1
 
     def get_exciter_bpm(self, plane, commonbpms):
-        if plane == "X":
-            if EXCITER_BPM in commonbpms:
-                return [list(commonbpms).index(EXCITER_BPM), EXCITER_BPM], "KIFBHN"
-        else:
-            if EXCITER_BPM in commonbpms:
-                return [list(commonbpms).index(EXCITER_BPM), EXCITER_BPM], "KIFBVN"
-        return KeyError
+        return [list(commonbpms).index(EXCITER_BPM), EXCITER_BPM], f"KIFB{PLANE_TO_HV[plane]}N"
