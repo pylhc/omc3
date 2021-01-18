8f02b364e1903ac0dee779d6c0e3e2a5a313d5aa
Maël Le Garrec <mael@legarrec.org>
2021-01-18 13:29:20 +0100

    Update for new keys

--- omc3/tbt/reader_sps.py
+++ omc3/tbt/reader_sps.py
@@ -25,11 +25,12 @@ PLANE_TO_NUM = {"X": "1", "Y": "90"}
 PLANE_CORRES = {"H": "X", "V": "Y"}
 
 # BINARY IDs
-N_BUNCHES = "BunchNos"
-N_TURNS = "nbOfTurns"
-TIMESTAMP = "timestampSecond"
-BPM_NAMES = "MonNames"
-CHANNEL_NAMES = "ChannelNames"
+N_BUNCHES = "nbOfCapBunches"
+N_TURNS = "nbOfCapTurns"
+TIMESTAMP = "acqStamp"
+BPM_NAMES = "bpmNames"
+VER_BPMS = "verBunchId"
+HOR_BPMS = "horBunchId"
 
 
 def read_tbt(file_path):
@@ -43,30 +44,18 @@ def read_tbt(file_path):
     """
     
     sdds_file = sdds.read(file_path)
-    date = datetime.utcfromtimestamp(sdds_file.values[TIMESTAMP]).replace(tzinfo=tz.tzutc())
-    nbunches = sdds_file.values[N_BUNCHES].max()
+    date = datetime.utcfromtimestamp(sdds_file.values[TIMESTAMP] / 1000).replace(tzinfo=tz.tzutc())
+    nbunches = sdds_file.values[N_BUNCHES]
     bpm_names = sdds_file.values[BPM_NAMES]
-    channel_names = sdds_file.values[CHANNEL_NAMES]
     nturns = sdds_file.values[N_TURNS]
     nbpms = len(bpm_names)
-    nchannels = len(channel_names)
 
     data = {'X': [], 'Y': []}
-    inverted_plane = {'X': 'Y', 'Y': 'X'}
-    for channel in channel_names:
-        # The last letter of a channel gives the plane, e.g. SPS.BPH.10208.H/H
-        plane = PLANE_CORRES[channel[-1]]  
+    for plane, key in ('X', HOR_BPMS), ('Y', VER_BPMS):
+        data[plane].extend(sdds_file.values[key])
+        data[plane] = np.reshape(data[plane], (nbpms, nbunches, nturns))
 
-        # Both planes are filled here to have the same matrices sizes
-        data[plane].extend(sdds_file.values[channel])
-        data[inverted_plane[plane]].extend([0] * len(sdds_file.values[channel]))
-
-    for plane in data.keys():
-        data[plane] = np.reshape(data[plane], (nchannels, nbunches, nturns))
-
-    # Remove the SPS. and .H/H or .V/V ad the end of the channel names
-    stripped_names = ['.'.join(s.split('.')[1:-1]) for s in channel_names]
-    matrices = [{k: pd.DataFrame(index=stripped_names,
+    matrices = [{k: pd.DataFrame(index=bpm_names,
                                  data=data[k][:, idx, :],
                                  dtype=float) for k in data} for idx in range(nbunches)]
 
