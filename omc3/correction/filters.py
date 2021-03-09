eb615cffcea07ac35dac8b3822f2c83c233ceb6c
Felix Soubelet <felix.soubelet@protonmail.com>
2021-03-09 20:24:18 +0100

    bit of type hinting for filters.py

--- omc3/correction/filters.py
+++ omc3/correction/filters.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
+from typing import Callable, Dict, Sequence
 
 import numpy as np
+import pandas as pd
 import tfs
 
 from omc3.correction.constants import DELTA, ERR, ERROR, VALUE, WEIGHT
@@ -9,7 +11,7 @@ from omc3.utils import logging_tools, stats
 LOG = logging_tools.get_logger(__name__)
 
 
-def filter_measurement(keys, meas, model, opt):
+def filter_measurement(keys: Sequence[str], meas: pd.DataFrame, model: pd.DataFrame, opt: dict) -> dict:
     """ Filters measurements and renames columns to VALUE, ERROR, WEIGHT"""
     filters = _get_measurement_filters()
     new = dict.fromkeys(keys)
@@ -18,11 +20,11 @@ def filter_measurement(keys, meas, model, opt):
     return new
 
 
-def _get_measurement_filters():
+def _get_measurement_filters() -> defaultdict:
     return defaultdict(lambda: _get_filtered_generic, {'Q': _get_tunes})
 
 
-def _get_filtered_generic(key, meas, model, opt):
+def _get_filtered_generic(key: str, meas: pd.DataFrame, model: pd.DataFrame, opt: dict) -> tfs.TfsDataFrame:
     common_bpms = meas.index.intersection(model.index)
     meas = meas.loc[common_bpms, :]
     new = tfs.TfsDataFrame(index=common_bpms)
@@ -47,7 +49,7 @@ def _get_filtered_generic(key, meas, model, opt):
     return new.loc[good_bpms, :]
 
 
-def _get_tunes(key, meas, model, opt):
+def _get_tunes(key: str, meas: pd.DataFrame, model, opt: dict):
     meas[WEIGHT] = opt.weights[key]
     if opt.use_errorbars:
         meas[WEIGHT] = _get_errorbased_weights(key, meas[WEIGHT], meas[ERROR])
@@ -55,7 +57,7 @@ def _get_tunes(key, meas, model, opt):
     return meas
 
 
-def _get_errorbased_weights(key, weights, errors):
+def _get_errorbased_weights(key: str, weights, errors):
     # TODO case without errors used may corrupt the correction (typical error != 1)
     w2 = stats.weights_from_errors(errors)
     if w2 is None:
@@ -65,40 +67,40 @@ def _get_errorbased_weights(key, weights, errors):
     return weights * np.sqrt(w2)
 
 
-def filter_response_index(response, measurement, keys):
-    not_in_response = [k for k in keys if k not in response]
+def filter_response_index(response, measurement, keys: Sequence[str]):
+    not_in_response = [key for key in keys if key not in response]
     if len(not_in_response) > 0:
         raise KeyError(f"The following optical parameters are not present in current"
                        f"response matrix: {not_in_response}")
 
     filters = _get_response_filters()
-    new_resp = {}
+    new_response = {}
     for key in keys:
-        new_resp[key] = filters[key](response[key], measurement[key])
-    return new_resp
+        new_response[key] = filters[key](response[key], measurement[key])
+    return new_response
 
 
-def _get_response_filters():
+def _get_response_filters() -> Dict[str, Callable]:
     return defaultdict(lambda:  _get_generic_response, {
         'MUX': _get_phase_response, 'MUY': _get_phase_response,
         'Q': _get_tune_response})
 
 
-def _get_generic_response(resp, meas):
+def _get_generic_response(resp: pd.DataFrame, meas: pd.DataFrame) -> pd.DataFrame:
     return resp.loc[meas.index.values, :]
 
 
-def _get_phase_response(resp, meas):
+def _get_phase_response(resp: pd.DataFrame, meas: pd.DataFrame) -> pd.DataFrame:
     phase1 = resp.loc[meas.index.values, :]
     phase2 = resp.loc[meas.loc[:, 'NAME2'].values, :]
     return -phase1.sub(phase2.values)  # phs2-phs1 but with idx of phs1
 
 
-def _get_tune_response(resp, meas):
+def _get_tune_response(resp, meas):  # ???
     return resp
 
 
-def _get_smallest_data_mask(data, portion=0.95):
+def _get_smallest_data_mask(data, portion: float = 0.95) -> np.ndarray:
     if not 0 <= portion <= 1:
         raise ValueError("Portion of data has to be between 0 and 1")
     b = int(len(data) * portion)
