44b040367f5f007f3b0580545c16eddedcb8bf73
Felix Soubelet <felix.soubelet@protonmail.com>
2021-03-15 11:45:58 +0100

    constants for response_madx

--- omc3/correction/response_madx.py
+++ omc3/correction/response_madx.py
@@ -20,6 +20,7 @@ import tfs
 from optics_functions.coupling import coupling_via_cmatrix
 
 import omc3.madx_wrapper as madx_wrapper
+from omc3.correction.constants import BETA, DISP, F1001, F1010, INCR, NORM_DISP, PHASE_ADV, TUNE
 from omc3.model.accelerators.accelerator import Accelerator
 from omc3.utils import iotools, logging_tools
 from omc3.utils.contexts import suppress_warnings, timeit
@@ -151,7 +152,7 @@ def _load_madx_results(
         vars_and_paths.append((value, temp_dir))
     var_to_twiss = {}
     for var, tfs_data in process_pool.map(_load_and_remove_twiss, vars_and_paths):
-        tfs_data['incr'] = incr_dict[var]
+        tfs_data[f"{INCR}"] = incr_dict[var]
         var_to_twiss[var] = tfs_data
     return var_to_twiss
 
@@ -160,54 +161,56 @@ def _create_fullresponse_from_dict(var_to_twiss: Dict[str, tfs.TfsDataFrame]) ->
     """ Convert var-tfs dictionary to fullresponse dictionary """
     var_to_twiss = _add_coupling(var_to_twiss)
     keys = list(var_to_twiss.keys())
-    columns = ["MUX","MUY","BETX","BETY","DX", "DY", "F1001R","F1001I","F1010R","F1010I","Q1","Q2","incr"]
-    
+    # columns = ["MUX","MUY","BETX","BETY","DX", "DY", "F1001R","F1001I","F1010R","F1010I","Q1","Q2","incr"]
+    columns = [f"{PHASE_ADV}X", f"{PHASE_ADV}Y", f"{BETA}X", f"{BETA}Y", f"{DISP}X", f"{DISP}Y",
+               f"{F1001}R", f"{F1001}I", f"{F1010}R", f"{F1010}I", f"{TUNE}1", f"{TUNE}2", f"{INCR}"]
+
     bpms = var_to_twiss["0"].index
-    resp = np.empty((len(keys),bpms.size,len(columns)))
+    resp = np.empty((len(keys), bpms.size, len(columns)))
     
     for i, key in enumerate(keys):
-        resp[i] = var_to_twiss[key].loc[:,columns].to_numpy()
+        resp[i] = var_to_twiss[key].loc[:, columns].to_numpy()
 
     resp = resp.transpose(2, 1, 0)
-    model_index = list(keys).index('0')
+    model_index = list(keys).index("0")
 
     # create normalized dispersion and dividing BET by nominal model
-    NDX_arr = np.divide(resp[columns.index("DX")],np.sqrt(resp[columns.index("BETX")]))
-    NDY_arr = np.divide(resp[columns.index("DY")],np.sqrt(resp[columns.index("BETY")]))
-    resp[columns.index("BETX")] = np.divide(resp[columns.index("BETX")],resp[columns.index("BETX"),:,model_index][:,np.newaxis])
-    resp[columns.index("BETY")] = np.divide(resp[columns.index("BETY")],resp[columns.index("BETY"),:,model_index][:,np.newaxis])
+    NDX_arr = np.divide(resp[columns.index(f"{DISP}X")], np.sqrt(resp[columns.index(f"{BETA}X")]))
+    NDY_arr = np.divide(resp[columns.index(f"{DISP}Y")], np.sqrt(resp[columns.index(f"{BETA}Y")]))
+    resp[columns.index(f"{BETA}X")] = np.divide(resp[columns.index(f"{BETA}X")], resp[columns.index(f"{BETA}X"), : , model_index][:, np.newaxis])
+    resp[columns.index(f"{BETA}Y")] = np.divide(resp[columns.index(f"{BETA}Y")], resp[columns.index(f"{BETA}Y"), : , model_index][:, np.newaxis])
 
     # subtracting nominal model from data
-    resp = np.subtract(resp, resp[:,:,model_index][:,:,np.newaxis])
-    NDX_arr = np.subtract(NDX_arr,NDX_arr[:,model_index][:,np.newaxis])
-    NDY_arr = np.subtract(NDY_arr,NDY_arr[:,model_index][:,np.newaxis])
+    resp = np.subtract(resp, resp[:, :, model_index][:, :, np.newaxis])
+    NDX_arr = np.subtract(NDX_arr, NDX_arr[:, model_index][:, np.newaxis])
+    NDY_arr = np.subtract(NDY_arr, NDY_arr[:, model_index][:, np.newaxis])
     
     # Remove difference of nominal model with itself (bunch of zeros) and divide by increment
-    resp = np.delete(resp,model_index,axis = 2)
-    NDX_arr = np.delete(NDX_arr,model_index,axis = 1)
-    NDY_arr = np.delete(NDY_arr,model_index,axis = 1)
+    resp = np.delete(resp, model_index, axis=2)
+    NDX_arr = np.delete(NDX_arr, model_index, axis=1)
+    NDY_arr = np.delete(NDY_arr, model_index, axis=1)
     keys.remove("0")
 
-    NDX_arr = np.divide(NDX_arr, resp[columns.index('incr')])
-    NDY_arr = np.divide(NDY_arr, resp[columns.index('incr')])
-    resp = np.divide(resp,resp[columns.index('incr')])
-    Q_arr = np.column_stack((resp[columns.index("Q1"),0,:],resp[columns.index("Q2"),0,:])).T   
+    NDX_arr = np.divide(NDX_arr, resp[columns.index(f"{INCR}")])
+    NDY_arr = np.divide(NDY_arr, resp[columns.index(f"{INCR}")])
+    resp = np.divide(resp,resp[columns.index(f"{INCR}")])
+    Q_arr = np.column_stack((resp[columns.index(f"{TUNE}1"), 0, :], resp[columns.index(f"{TUNE}2"), 0, :])).T
  
     with suppress_warnings(np.ComplexWarning):  # raised as everything is complex-type now
         return {
-            'MUX': pd.DataFrame(data=resp[columns.index("MUX")], index=bpms, columns=keys).astype(np.float64),
-            'MUY': pd.DataFrame(data=resp[columns.index("MUY")], index=bpms, columns=keys).astype(np.float64),
-            'BETX': pd.DataFrame(data=resp[columns.index("BETX")], index=bpms, columns=keys).astype(np.float64),
-            'BETY': pd.DataFrame(data=resp[columns.index("BETY")], index=bpms, columns=keys).astype(np.float64),
-            'DX': pd.DataFrame(data=resp[columns.index("DX")], index=bpms, columns=keys).astype(np.float64),
-            'DY': pd.DataFrame(data=resp[columns.index("DY")], index=bpms, columns=keys).astype(np.float64),
-            'NDX': pd.DataFrame(data=NDX_arr, index=bpms, columns=keys).astype(np.float64),
-            'NDY': pd.DataFrame(data=NDY_arr, index=bpms, columns=keys).astype(np.float64),
-            "F1001R": pd.DataFrame(data=resp[columns.index('F1001R')], index=bpms, columns=keys).astype(np.float64),
-            "F1001I": pd.DataFrame(data=resp[columns.index('F1001I')], index=bpms, columns=keys).astype(np.float64),
-            "F1010R": pd.DataFrame(data=resp[columns.index('F1010R')], index=bpms, columns=keys).astype(np.float64),
-            "F1010I": pd.DataFrame(data=resp[columns.index('F1010I')], index=bpms, columns=keys).astype(np.float64),
-            'Q': pd.DataFrame(data=Q_arr, index=["Q1", "Q2"], columns=keys).astype(np.float64),
+            f"{PHASE_ADV}X": pd.DataFrame(data=resp[columns.index(f"{PHASE_ADV}X")], index=bpms, columns=keys).astype(np.float64),
+            f"{PHASE_ADV}Y": pd.DataFrame(data=resp[columns.index(f"{PHASE_ADV}Y")], index=bpms, columns=keys).astype(np.float64),
+            f"{BETA}X": pd.DataFrame(data=resp[columns.index(f"{BETA}X")], index=bpms, columns=keys).astype(np.float64),
+            f"{BETA}Y": pd.DataFrame(data=resp[columns.index(f"{BETA}Y")], index=bpms, columns=keys).astype(np.float64),
+            f"{DISP}X": pd.DataFrame(data=resp[columns.index(f"{DISP}X")], index=bpms, columns=keys).astype(np.float64),
+            f"{DISP}Y": pd.DataFrame(data=resp[columns.index(f"{DISP}Y")], index=bpms, columns=keys).astype(np.float64),
+            f"{NORM_DISP}X": pd.DataFrame(data=NDX_arr, index=bpms, columns=keys).astype(np.float64),
+            f"{NORM_DISP}Y": pd.DataFrame(data=NDY_arr, index=bpms, columns=keys).astype(np.float64),
+            f"{F1001}R": pd.DataFrame(data=resp[columns.index(f"{F1001}R")], index=bpms, columns=keys).astype(np.float64),
+            f"{F1001}I": pd.DataFrame(data=resp[columns.index(f"{F1001}I")], index=bpms, columns=keys).astype(np.float64),
+            f"{F1010}R": pd.DataFrame(data=resp[columns.index(f"{F1010}R")], index=bpms, columns=keys).astype(np.float64),
+            f"{F1010}I": pd.DataFrame(data=resp[columns.index(f"{F1010}I")], index=bpms, columns=keys).astype(np.float64),
+            f"{TUNE}": pd.DataFrame(data=Q_arr, index=[f"{TUNE}1", f"{TUNE}2"], columns=keys).astype(np.float64),
         }
 
 
@@ -228,8 +231,8 @@ def _load_and_remove_twiss(var_and_path: Tuple[str, str]) -> Tuple[str, tfs.TfsD
     (var, path) = var_and_path
     twissfile = os.path.join(path, "twiss." + var)
     tfs_data = tfs.read(twissfile, index="NAME")
-    tfs_data['Q1'] = tfs_data.Q1
-    tfs_data['Q2'] = tfs_data.Q2
+    tfs_data[f"{TUNE}1"] = tfs_data.Q1
+    tfs_data[f"{TUNE}2"] = tfs_data.Q2
     os.remove(twissfile)
     return var, tfs_data
 
@@ -250,8 +253,8 @@ def _add_coupling(dict_of_tfs: Dict[str, tfs.TfsDataFrame]) -> Dict[str, tfs.Tfs
     with timeit(lambda elapsed: LOG.debug(f"  Time adding coupling: {elapsed} s")):
         for var, tfs_dframe in result_dict_of_tfs.items():  # already copies, so it's safe to act on them
             coupling_rdts_df = coupling_via_cmatrix(tfs_dframe)
-            tfs_dframe["F1001R"] = np.real(coupling_rdts_df["F1001"]).astype(np.float64)
-            tfs_dframe["F1001I"] = np.imag(coupling_rdts_df["F1001"]).astype(np.float64)
-            tfs_dframe["F1010R"] = np.real(coupling_rdts_df["F1010"]).astype(np.float64)
-            tfs_dframe["F1010I"] = np.imag(coupling_rdts_df["F1010"]).astype(np.float64)
+            tfs_dframe[f"{F1001}R"] = np.real(coupling_rdts_df[f"{F1001}"]).astype(np.float64)
+            tfs_dframe[f"{F1001}I"] = np.imag(coupling_rdts_df[f"{F1001}"]).astype(np.float64)
+            tfs_dframe[f"{F1010}R"] = np.real(coupling_rdts_df[f"{F1010}"]).astype(np.float64)
+            tfs_dframe[f"{F1010}I"] = np.imag(coupling_rdts_df[f"{F1010}"]).astype(np.float64)
         return result_dict_of_tfs
