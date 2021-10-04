56453e2e8ed9291ce14f9dab5deac50b5f379534
Felix Soubelet <felix.soubelet@protonmail.com>
2021-10-04 16:55:13 +0200

    fix indirectly broken import

--- omc3/scripts/update_nattune_in_linfile.py
+++ omc3/scripts/update_nattune_in_linfile.py
@@ -48,8 +48,8 @@ from generic_parser import entrypoint, EntryPointParameters
 from generic_parser.entrypoint_parser import save_options_to_config
 
 from omc3.definitions import formats
-from omc3.definitions.constants import COL_NATTUNE, COL_NATAMP, FILE_LIN_EXT, NAME, PLANES
-from omc3.plotting.spectrum.utils import load_spectrum_data, get_bpms, LIN, AMPS, FREQ
+from omc3.definitions.constants import COL_NATTUNE, COL_NATAMP, FILE_LIN_EXT, FREQ, NAME, PLANES
+from omc3.plotting.spectrum.utils import load_spectrum_data, get_bpms, LIN, AMPS
 from omc3.utils.logging_tools import get_logger, list2str
 
 LOG = get_logger(__name__)
