a9f263a9f24c1ea4d46e1a21abc65b40bea83224
Felix Soubelet <felix.soubelet@protonmail.com>
2021-10-08 11:01:18 +0200

    tests adapted to new turn_by_turn

--- tests/unit/test_tbt_converter.py
+++ tests/unit/test_tbt_converter.py
@@ -1,26 +1,21 @@
-import tempfile
-from datetime import datetime
 from pathlib import Path
 
-import h5py
 import numpy as np
-import pandas as pd
 import pytest
-
-from turn_by_turn.constants import PRINT_PRECISION
+import turn_by_turn as tbt
 
 from omc3.definitions.constants import PLANES
 from omc3.tbt_converter import converter_entrypoint
 
 INPUTS_DIR = Path(__file__).parent.parent / "inputs"
-ASCII_PRECISION = 0.5 / np.power(10, PRINT_PRECISION)
+ASCII_PRECISION = 0.5 / np.power(10, tbt.constants.PRINT_PRECISION)
 
 
 @pytest.mark.basic
 def test_converter_one_file(_sdds_file, _test_file):
     converter_entrypoint(files=[_sdds_file], outputdir=_test_file.parent)
-    origin = handler.read_tbt(_sdds_file)
-    new = handler.read_tbt(f"{_test_file}.sdds")
+    origin = tbt.read_tbt(_sdds_file)
+    new = tbt.read_tbt(f"{_test_file}.sdds")
     _compare_tbt(origin, new, False)
 
 
@@ -32,16 +27,18 @@ def test_converter_drop_elements(_sdds_file, _test_file, dropped_elements):
         outputdir=_test_file.parent,
         drop_elements=dropped_elements,
     )
-    new = handler.read_tbt(f"{_test_file}.sdds")
-    for dictionary in new.matrices:
-        for dataframe in dictionary.values():
+    new = tbt.read_tbt(f"{_test_file}.sdds")
+    for transverse_data in new.matrices:
+        for dataframe in (transverse_data.X, transverse_data.Y):
             for element in dropped_elements:
                 assert element not in dataframe.index
 
 
 @pytest.mark.basic
 @pytest.mark.parametrize("unknown_element", ["NOT_IN_DATA", "QBX.P4.T1", "INVALID"])
-def test_converter_warns_on_not_found_drop_elements(_sdds_file, _test_file, unknown_element, caplog):
+def test_converter_warns_on_not_found_drop_elements(
+    _sdds_file, _test_file, unknown_element, caplog
+):
     converter_entrypoint(
         files=[_sdds_file],
         outputdir=_test_file.parent,
@@ -55,8 +52,8 @@ def test_converter_one_file_with_noise(_sdds_file, _test_file):
     np.random.seed(2019)
     noiselevel = 0.0005
     converter_entrypoint(files=[_sdds_file], outputdir=_test_file.parent, noise_levels=[noiselevel])
-    origin = handler.read_tbt(_sdds_file)
-    new = handler.read_tbt(f"{_test_file}_n{noiselevel}.sdds")
+    origin = tbt.read_tbt(_sdds_file)
+    new = tbt.read_tbt(f"{_test_file}_n{noiselevel}.sdds")
     _compare_tbt(origin, new, True, noiselevel * 10)
 
 
@@ -64,9 +61,9 @@ def test_converter_one_file_with_noise(_sdds_file, _test_file):
 def test_converter_more_files(_sdds_file, _test_file):
     rep = 2
     converter_entrypoint(files=[_sdds_file], outputdir=_test_file.parent, realizations=rep)
-    origin = handler.read_tbt(_sdds_file)
+    origin = tbt.read_tbt(_sdds_file)
     for i in range(rep):
-        new = handler.read_tbt(f"{_test_file}_r{i}.sdds")
+        new = tbt.read_tbt(f"{_test_file}_r{i}.sdds")
         _compare_tbt(origin, new, False)
 
 
@@ -78,14 +75,14 @@ def test_converter_more_files_with_noise(_sdds_file, _test_file):
     converter_entrypoint(
         files=[_sdds_file], outputdir=_test_file.parent, realizations=rep, noise_levels=[noiselevel]
     )
-    origin = handler.read_tbt(_sdds_file)
+    origin = tbt.read_tbt(_sdds_file)
     for i in range(rep):
-        new = handler.read_tbt(f"{_test_file}_n{noiselevel}_r{i}.sdds")
+        new = tbt.read_tbt(f"{_test_file}_n{noiselevel}_r{i}.sdds")
         _compare_tbt(origin, new, True, noiselevel * 10)
 
 
 def _compare_tbt(
-    origin: handler.TbtData, new: handler.TbtData, no_binary: bool, max_deviation=ASCII_PRECISION
+    origin: tbt.TbtData, new: tbt.TbtData, no_binary: bool, max_deviation=ASCII_PRECISION
 ) -> None:
     assert new.nturns == origin.nturns
     assert new.nbunches == origin.nbunches
@@ -99,3 +96,13 @@ def _compare_tbt(
                 assert np.max(np.abs(origin_mat - new_mat)) < max_deviation
             else:
                 assert np.all(origin_mat == new_mat)
+
+
+@pytest.fixture()
+def _sdds_file() -> Path:
+    return INPUTS_DIR / "test_file.sdds"
+
+
+@pytest.fixture()
+def _test_file(tmp_path) -> Path:
+    yield tmp_path / "test_file"
