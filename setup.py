2194fee08632cba663cb89cc7990b2e072ce64e5
Felix Soubelet <felix.soubelet@protonmail.com>
2021-10-28 17:32:45 +0200

    update tbt dependency to working version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ DEPENDENCIES = [
     "tables>=3.6.0",
     "uncertainties>=3.1.4",
     "optics-functions>=0.1.0",
-    "turn_by_turn>=0.1.0",
+    "turn_by_turn>=0.2.0",
 ]
 
 # Extra dependencies
