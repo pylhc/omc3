e86ac362654b4b642bb25373558aec47c73942e8
JoschD <26184899+JoschD@users.noreply.github.com>
2021-10-31 16:22:05 +0000

    Adapt BBQ extraction for NXCALS in amplitude detuning analysis(#349)
    Co-authored-by: Felix Soubelet <felix.soubelet@protonmail.com>

--- omc3/tune_analysis/timber_extract.py
+++ omc3/tune_analysis/timber_extract.py
@@ -12,15 +12,19 @@ dependency and installing from the ``acc-py`` package index (by specifying ``--i
 https://acc-py-repo.cern.ch/repository/vr-py-releases/simple`` and
 ``--trusted-host acc-py-repo.cern.ch`` to your ``pip`` installation command).
 """
+import datetime
 import re
+from contextlib import suppress
+from typing import Dict, List, NewType, Sequence, Tuple, Union
 
 import numpy as np
 import tfs
+from jpype import JException, java
 
 from omc3.tune_analysis import constants as const
 from omc3.utils import logging_tools
-from omc3.utils.time_tools import CERNDatetime
 from omc3.utils.mock import cern_network_import
+from omc3.utils.time_tools import CERNDatetime
 
 TIME_COL = const.get_time_col()
 START_TIME = const.get_tstart_head()
@@ -29,80 +33,111 @@ END_TIME = const.get_tend_head()
 LOG = logging_tools.get_logger(__name__)
 pytimber = cern_network_import("pytimber")
 
+MAX_RETRIES = 10  # number of retries on retryable exception
+AcceptableTimeStamp = NewType("AcceptableTimeStamp", Union[CERNDatetime, int, float])
 
-def lhc_fill_to_tfs(fill_number, keys=None, names=None) -> tfs.TfsDataFrame:
+
+def lhc_fill_to_tfs(
+    fill_number: int, keys: Sequence[str] = None, names: Dict[str, str] = None
+) -> tfs.TfsDataFrame:
     """
     Extracts data for keys of fill from ``Timber``.
 
     Args:
-        fill_number: fill number.
-        keys: list of data to extract.
-        names: dict to map keys to column names.
+        fill_number (int): Number of the fill to extract from.
+        keys (Sequence[str]): the different variables names to extract data for.
+        names (Dict[str, str): dict mapping keys to column names.
 
-    Returns: tfs pandas dataframe.
+    Returns:
+        The extracted data as a ``TfsDataFrame``.
     """
     db = pytimber.LoggingDB(source="nxcals")
     t_start, t_end = get_fill_times(db, fill_number)
-    out_df = extract_between_times(t_start, t_end, keys, names)
-    return out_df
+    return extract_between_times(t_start, t_end, keys, names)
 
 
-def extract_between_times(t_start, t_end, keys=None, names=None) -> tfs.TfsDataFrame:
+def extract_between_times(
+    t_start: AcceptableTimeStamp,
+    t_end: AcceptableTimeStamp,
+    keys: Sequence[str] = None,
+    names: Dict[str, str] = None
+) -> tfs.TfsDataFrame:
     """
-    Extracts data for keys between t_start and t_end from timber.
+    Extracts data for keys between ``t_start`` and ``t_end`` from ``Timber``.
 
     Args:
-        t_start: starting time in local time or timestamp.
-        t_end: end time in local time or timestamp.
-        keys: list of data to extract.
-        names: dict to map keys to column names.
+        t_start (AcceptableTimeStamp): starting time in CERNDateTime or timestamp.
+        t_end (AcceptableTimeStamp): end time in local CERNDateTime or timestamp.
+        keys (Sequence[str]): the different variables names to extract data for.
+        names (Dict[str, str): dict mapping keys to column names.
 
-    Returns: tfs pandas dataframe.
+    Returns:
+        Extracted data in a ``TfsDataFrame``.
     """
+    with suppress(TypeError):
+        t_start: CERNDatetime = CERNDatetime.from_timestamp(t_start)
+
+    with suppress(TypeError):
+        t_end: CERNDatetime = CERNDatetime.from_timestamp(t_end)
+
     db = pytimber.LoggingDB(source="nxcals")
     if keys is None:
         keys = get_tune_and_coupling_variables(db)
 
-    extract_dict = db.get(keys, t_start, t_end)
+    # Attempt getting data from NXCALS, which can sometimes need a few retries (yay NXCALS)
+    # If Java gives a feign.RetryableException, retry up to MAX_RETRIES times.
+    for tries in range(MAX_RETRIES + 1):
+        try:
+            # We use timestamps to avoid any confusion with local time
+            extract_dict = db.get(keys, t_start.timestamp(), t_end.timestamp())
+        except java.lang.IllegalStateException as e:
+            raise IOError("Could not get data from Timber, user probably has no access to NXCALS") from e
+        except JException as e:  # Might be a case for retries
+            if "RetryableException" in str(e) and (tries + 1) < MAX_RETRIES:
+                LOG.warning(f"Could not get data from Timber! Trial no {tries + 1} / {MAX_RETRIES}")
+                continue  # will go to the next iteratoin of the loop, so retry
+            raise IOError("Could not get data from timber!") from e
+        else:
+            break
 
     out_df = tfs.TfsDataFrame()
     for key in keys:
-        if extract_dict[key.upper()][1][0].size > 1:
-            raise NotImplementedError("Multidimensional variables are not implemented yet.")
+        if extract_dict[key][1][0].size > 1:
+            raise NotImplementedError("Multidimensional variables are not implemented yet")
 
-        data = np.asarray(extract_dict[key.upper()]).transpose()
-        col = names.get(key, key)
-
-        key_df = tfs.TfsDataFrame(data, columns=[TIME_COL, col]).set_index(TIME_COL)
-
+        data = np.asarray(extract_dict[key]).transpose()
+        column = key if names is None else names.get(key, key)
+        key_df = tfs.TfsDataFrame(data, columns=[TIME_COL, column]).set_index(TIME_COL)
         out_df = out_df.merge(key_df, how="outer", left_index=True, right_index=True)
 
     out_df.index = [CERNDatetime.from_timestamp(i) for i in out_df.index]
-    out_df.headers[START_TIME] = CERNDatetime.from_timestamp(t_start).cern_utc_string()
-    out_df.headers[END_TIME] = CERNDatetime.from_timestamp(t_end).cern_utc_string()
+    out_df.headers[START_TIME] = t_start.cern_utc_string()
+    out_df.headers[END_TIME] = t_end.cern_utc_string()
     return out_df
 
 
-def get_tune_and_coupling_variables(db) -> list:
+def get_tune_and_coupling_variables(db) -> List[str]:
     """
     Returns the tune and coupling variable names.
 
     Args:
-        db (pytimber.LoggingDB): pytimber database.
+        db (pytimber.LoggingDB): pytimber database connexion.
 
     Returns:
-        `list` of variable names.
+        List of variable names as strings.
     """
     bbq_vars = []
-    for search_term in ['%EIGEN%FREQ%', '%COUPL%ABS%']:
+    for search_term in ["%EIGEN%FREQ%", "%COUPL%ABS%"]:
         search_results = db.search(search_term)
         for res in search_results:
-            if re.match(r'LHC\.B(OFSU|QBBQ\.CONTINUOUS)', res):
+            if re.match(r"LHC\.B(OFSU|QBBQ\.CONTINUOUS)", res):
                 bbq_vars.append(res)
     return bbq_vars
 
 
-def get_fill_times(db, fill_number: int) -> tuple:
+def get_fill_times(
+    db, fill_number: int
+) -> Tuple[Union[datetime.datetime, float], Union[datetime.datetime, float]]:
     """
     Returns start and end time of fill with fill number.
 
@@ -114,4 +149,4 @@ def get_fill_times(db, fill_number: int) -> tuple:
        `Tuple` of start and end time.
     """
     fill = db.getLHCFillData(fill_number)
-    return fill['startTime'], fill['endTime']
+    return fill["startTime"], fill["endTime"]
