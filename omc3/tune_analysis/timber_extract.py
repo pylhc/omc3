728d0e08606b54c364780646df3a4eed2253db29
Felix Soubelet <felix.soubelet@protonmail.com>
2021-10-31 14:54:04 +0100

    add missing Tuple import

--- omc3/tune_analysis/timber_extract.py
+++ omc3/tune_analysis/timber_extract.py
@@ -15,7 +15,7 @@ https://acc-py-repo.cern.ch/repository/vr-py-releases/simple`` and
 import datetime
 import re
 from contextlib import suppress
-from typing import Dict, List, NewType, Sequence, Union
+from typing import Dict, List, NewType, Sequence, Tuple, Union
 
 import numpy as np
 import tfs
