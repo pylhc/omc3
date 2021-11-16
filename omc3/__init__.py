518557b4f1f49913944de4fe7cbd88e665c0cb48
JoschD <26184899+JoschD@users.noreply.github.com>
2021-11-16 15:36:02 +0000

    fixed zenodo, added people, moved things around (#350)
    * fixed zenodo
    * zenodo: added JK and HGM
    * zenodo: changed order as in github contributors
    * Changed license in `__init__.py`

--- omc3/__init__.py
+++ omc3/__init__.py
@@ -5,13 +5,13 @@ The omc3 library
 omc3 is a tool package for the optics measurements and corrections group (OMC) at CERN.
 
 :copyright: pyLHC/OMC-Team working group.
-:license: GNU GPLv3, see the LICENSE file for details.
+:license: MIT, see the LICENSE file for details.
 """
 
 __title__ = "omc3"
 __description__ = "An accelerator physics tools package for the OMC team at CERN."
 __url__ = "https://github.com/pylhc/omc3"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "pylhc"
 __author_email__ = "pylhc@github.com"
 __license__ = "MIT"
