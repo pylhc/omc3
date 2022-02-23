566195228ac7a92f9b76b964c2cbe1c5025d2e39
Felix Soubelet <felix.soubelet@protonmail.com>
2022-02-23 10:04:40 +0100

    add docstrings with info on the filtering functions

--- omc3/correction/filters.py
+++ omc3/correction/filters.py
@@ -21,13 +21,9 @@ import pandas as pd
 import tfs
 from generic_parser import DotDict
 
-from omc3.correction.constants import (
-    NAME2, DELTA, ERR, ERROR, PHASE, TUNE, VALUE, WEIGHT, PHASE_ADV
-)
+from omc3.correction.constants import DELTA, ERR, ERROR, NAME2, PHASE, PHASE_ADV, TUNE, VALUE, WEIGHT
 from omc3.definitions.constants import PLANES
-from omc3.optics_measurements.constants import (
-    REAL, IMAG, AMPLITUDE, F1001, F1010
-)
+from omc3.optics_measurements.constants import AMPLITUDE, F1001, F1010, IMAG, REAL
 from omc3.utils import logging_tools, stats
 
 LOG = logging_tools.get_logger(__name__)
@@ -36,8 +32,10 @@ LOG = logging_tools.get_logger(__name__)
 # Measurement Filter -----------------------------------------------------------
 
 
-def filter_measurement(keys: Sequence[str], meas: Dict[str, pd.DataFrame], model: pd.DataFrame, opt: DotDict) -> dict:
-    """ Filters measurements in `keys` based on the dict-entries (keys as in `keys`)
+def filter_measurement(
+    keys: Sequence[str], meas: Dict[str, pd.DataFrame], model: pd.DataFrame, opt: DotDict
+) -> dict:
+    """Filters measurements in `keys` based on the dict-entries (keys as in `keys`)
     in `opt.errorcut`, `opt.modelcut` and `opt.weights` and unifies the
     data-column names to VALUE, ERROR, WEIGHT.
     If `opt.use_errorbars` is `True` the weights will be also based on the errors."""
@@ -49,19 +47,39 @@ def filter_measurement(keys: Sequence[str], meas: Dict[str, pd.DataFrame], model
 
 
 def _get_measurement_filters() -> defaultdict:
-    """ Returns a dict with the respective `_get_*` filter-functions that defaults
-    to `_get_filtered_generic`."""
-    return defaultdict(lambda: _get_filtered_generic, {f"{TUNE}": _get_tunes,
-                                                       f"{F1010}I": _get_coupling,
-                                                       f"{F1010}R": _get_coupling,
-                                                       f"{F1001}I": _get_coupling,
-                                                       f"{F1001}R": _get_coupling,
-                                                       })
+    """Returns a dict with the respective `_get_*` filter-functions that defaults
+    to `~_get_filtered_generic`. Some columns might need to have extra steps for
+    filtering, or simply a different filtering process."""
+    return defaultdict(
+        lambda: _get_filtered_generic,
+        {
+            f"{TUNE}": _get_tunes,
+            f"{F1010}I": _get_coupling,
+            f"{F1010}R": _get_coupling,
+            f"{F1001}I": _get_coupling,
+            f"{F1001}R": _get_coupling,
+        },
+    )
 
 
 def _get_filtered_generic(col: str, meas: pd.DataFrame, model: pd.DataFrame, opt: DotDict) -> tfs.TfsDataFrame:
+    """
+    Filters the provided column *col* of the measurement dataframe *meas*, based on the model values
+    (from the *model* dataframe) and the filtering options given at the command line (for instance,
+    the ``errorcut`` and ``modelcut`` values).
+
+    Args:
+        col (str): The column name to filter.
+        meas (pd.DataFrame): The measurement dataframe
+        model (pd.DataFrame): The model dataframe, which we get from the ``model_creator``.
+        opt (DotDict): The command line options dictionary.
+
+    Returns:
+        The filtered dataframe as a `~tfs.TfsDataFrame`.
+    """
     common_bpms = meas.index.intersection(model.index)
     meas = meas.loc[common_bpms, :]
+
     new = tfs.TfsDataFrame(index=common_bpms)
     new[VALUE] = meas.loc[:, col].to_numpy()
     new[ERROR] = meas.loc[:, f"{ERR}{col}"].to_numpy()
@@ -70,7 +88,8 @@ def _get_filtered_generic(col: str, meas: pd.DataFrame, model: pd.DataFrame, opt
         if opt.use_errorbars
         else opt.weights[col]
     )
-    # filter cuts
+
+    # Applying filtering cuts
     error_filter = meas.loc[:, f"{ERR}{DELTA}{col}"].to_numpy() < opt.errorcut[col]
     model_filter = np.abs(meas.loc[:, f"{DELTA}{col}"].to_numpy()) < opt.modelcut[col]
     # if opt.automatic_model_cut:  # TODO automated model cut
@@ -82,7 +101,7 @@ def _get_filtered_generic(col: str, meas: pd.DataFrame, model: pd.DataFrame, opt
         good_bpms[-1] = False
     else:
         good_bpms = error_filter & model_filter
-    LOG.debug(f"Number of BPMs with {col}: {np.sum(good_bpms)}")
+    LOG.debug(f"Number of BPMs kept for column '{col}' after filtering: {np.sum(good_bpms)}")
     return new.loc[good_bpms, :]
 
 
@@ -95,6 +114,23 @@ def _get_tunes(key: str, meas: pd.DataFrame, model, opt: DotDict):
 
 
 def _get_coupling(col: str, meas: pd.DataFrame, model: pd.DataFrame, opt: DotDict) -> tfs.TfsDataFrame:
+    """
+    Applies filters to the coupling dataframe *meas*. This is a bit hacky. Takes the measurement and
+    model dataframes for one of the coupling RDTs (*meas* comes from **f1001.tfs** or **f1010.tfs**)
+    maps the column *col* name to the "old" naming (for the F1001 for instance, REAL -> F1001R)
+    before passing to `~_get_filtered_generic` which will do the filtering. This is because
+    `~_get_filtered_generic` does a comparison to the model which has old names.
+
+    Args:
+        col (str): The column name to filter.
+        meas (pd.DataFrame): The measurement dataframe, which here will be the loaded **f1001.tfs**
+            or **f1010.tfs** file.
+        model (pd.DataFrame): The model dataframe, which we get from the ``model_creator``.
+        opt (DotDict): The command line options dictionary.
+
+    Returns:
+        The filtered dataframe as a `~tfs.TfsDataFrame`.
+    """
     # rename measurement column to key
     column_map = {c[0]: c for c in [REAL, IMAG, AMPLITUDE, PHASE]}  # only REAL and IMAG implemented in responses so far
     meas_col = column_map[col[-1]]
@@ -116,16 +152,19 @@ def _get_errorbased_weights(key: str, weights, errors):
 
 # Response Matrix Filter -------------------------------------------------------
 
+
 def filter_response_index(response: Dict, measurement: Dict, keys: Sequence[str]):
-    """ Filters the index of the response matrices `response` by the respective entries in `measurement`. """
+    """Filters the index of the response matrices `response` by the respective entries in `measurement`."""
     # rename MU to PHASE as we create a PHASE-Response afterwards
     # easier to do here, than to check eveywhere below. (jdilly)
     _rename_phase_advance(response)
 
     not_in_response = [key for key in keys if key not in response]
     if len(not_in_response) > 0:
-        raise KeyError(f"The following optical parameters are not present in current"
-                       f"response matrix: {not_in_response}")
+        raise KeyError(
+            f"The following optical parameters are not present in current"
+            f"response matrix: {not_in_response}"
+        )
 
     filters = _get_response_filters()
     new_response = {}
@@ -135,8 +174,10 @@ def filter_response_index(response: Dict, measurement: Dict, keys: Sequence[str]
 
 
 def _get_response_filters() -> Dict[str, Callable]:
-    """ Returns a dict with the respective `_get_*_response` functions that defaults
-    to `_get_generic_response`."""
+    """
+    Returns a dict with the respective `_get_*_response` functions that defaults
+    to `_get_generic_response`.
+    """
     return defaultdict(
         lambda: _get_generic_response,
         {f"{PHASE}X": _get_phase_response, f"{PHASE}Y": _get_phase_response},
@@ -164,7 +205,7 @@ def _get_smallest_data_mask(data, portion: float = 0.95) -> np.ndarray:
 
 
 def _rename_phase_advance(response):
-    """ Renames MU to PHASE inplace. """
+    """Renames MU to PHASE inplace."""
     for plane in PLANES:
         try:
             response[f"{PHASE}{plane}"] = response.pop(f"{PHASE_ADV}{plane}")
