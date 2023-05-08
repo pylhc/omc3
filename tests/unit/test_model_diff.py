956ee473699bd36527fa2a969ccff1d18c5c809b
JoschD <26184899+JoschD@users.noreply.github.com>
2023-05-09 01:35:57 +0200

    more import fixes and deprecation warning

--- tests/unit/test_model_diff.py
+++ tests/unit/test_model_diff.py
@@ -4,12 +4,13 @@ import numpy as np
 import tfs
 import pytest
 
-from omc3.correction.constants import DELTA, BETA, TUNE, NORM_DISPERSION, DISPERSION, PHASE_ADV, PHASE
 from omc3.correction.model_appenders import add_coupling_to_model
 from omc3.correction.model_diff import diff_twiss_parameters
 from omc3.global_correction import OPTICS_PARAMS_CHOICES
 from omc3.optics_measurements.toolbox import ang_diff, ang_sum
-from omc3.optics_measurements.constants import NAME
+from omc3.optics_measurements.constants import (
+    NAME, DELTA, BETA, TUNE, NORM_DISPERSION, DISPERSION, PHASE_ADV, PHASE
+)
 
 INPUTS = Path(__file__).parent.parent / 'inputs'
 
