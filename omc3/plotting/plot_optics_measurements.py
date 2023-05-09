e47e5928d5f25d3570da1f3d940e7fcfe0c72b32
JoschD <26184899+JoschD@users.noreply.github.com>
2023-05-09 02:01:38 +0200

    fix plot optics meas

--- omc3/plotting/plot_optics_measurements.py
+++ omc3/plotting/plot_optics_measurements.py
@@ -356,7 +356,7 @@ def _plot_rdt(optics_parameter, files, file_labels, x_column, x_label, ip_positi
 
 def _get_rdt_columns():
     result = {key: [None] * len(RDT_COLUMN_MAPPING) for key in ['y_columns', 'y_labels', 'error_columns']}
-    for idx, col_map in enumerate(RDT_COLUMN_MAPPING.items()):
+    for idx, col_map in enumerate(RDT_COLUMN_MAPPING.values()):
         result['y_columns'][idx] = col_map.column
         result['error_columns'][idx] = col_map.error_column  # should now all be there in the files
         result['y_labels'][idx] = col_map.ylabel
