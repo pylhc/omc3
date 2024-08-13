3d94badcc13abe864d07ad7c44f94d81eb1c5b57
Felix Soubelet <felix.soubelet@protonmail.com>
2024-08-13 15:22:18 +0200

    reinstate re-exports with explanation and lint disable

--- omc3/plotting/optics_measurements/constants.py
+++ omc3/plotting/optics_measurements/constants.py
@@ -4,7 +4,15 @@ Constants
 
 Constants and definitions for the ``plotting`` module.
 """
-
+# These below are here so they can be imported from the plotting
+# module of omc3 directly and not care where they originate / avoid
+# re-defining somewhere.
+from omc3.optics_measurements.constants import (  # noqa: F401
+    S, ERR, DELTA, AMPLITUDE, BETA, PHASE, REAL, IMAG,
+    AMP_BETA_NAME, BETA_NAME, CHROM_BETA_NAME, PHASE_NAME,  
+    SPECIAL_PHASE_NAME, TOTAL_PHASE_NAME, DISPERSION_NAME,
+    NORM_DISP_NAME, ORBIT_NAME, KICK_NAME, IP_NAME, PHASE_ADV, MDL
+)
 
 IP_POS_DEFAULT = {
     "LHCB1": {
