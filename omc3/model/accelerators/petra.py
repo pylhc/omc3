d86a8682c733e12b6ca133a9b502ca4238bbdd98
lmalina <lukas.malina@pm.me>
2024-09-11 13:37:45 +0200

    Bug fixes

--- omc3/model/accelerators/petra.py
+++ omc3/model/accelerators/petra.py
@@ -89,9 +89,10 @@ class Petra(Accelerator):
         self.beta = opt.beta
 
     def verify_object(self):  # TODO: Maybe more checks?
-        if self.model_dir is None:  # is the class is used to create full response?
-            raise AcceleratorDefinitionError("PETRA doesn't have a model creation yet, calling it this "
-                                             "way is most probably wrong.")
+        pass
+        # if self.model_dir is None:  # is the class is used to create full response?
+        #     raise AcceleratorDefinitionError("PETRA doesn't have a model creation yet, calling it this "
+        #                                      "way is most probably wrong.")
 
     def get_exciter_bpm(self, plane, commonbpms):
         return [list(commonbpms).index(EXCITER_BPM), EXCITER_BPM], f"KIFB{PLANE_TO_HV[plane]}N"
