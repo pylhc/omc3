879cbe920d281c0b4c201dad2f6c403a22efc771
Joshua Gray <61458311+jgray-19@users.noreply.github.com>
2025-08-27 16:37:19 +0200

    Refactor natural tune validation in harpy_per_plane function for improved error handling

--- omc3/harpy/frequency.py
+++ omc3/harpy/frequency.py
@@ -115,13 +115,7 @@ def harpy_per_plane(harpy_input, bpm_matrix, usv, tunes, plane):
     nattunes = _get_natural_tunes(harpy_input, tunes)
     tune_tol = harpy_input.tolerance
     if nattunes is not None:
-        if any(abs(t - n) < tune_tol for n, t in zip(nattunes[:2], tunes[:2])):
-            if all(n == t for n, t in zip(nattunes[:2], tunes[:2])):
-                # Different error to try and be more informative
-                raise ValueError(
-                    "Natural tunes match driven tunes. "
-                    "If analysing a simulation without an ACD don't give the nattunes or natdeltas."
-                )
+        if any(abs(t - n) < tune_tol and n != 0 for n, t in zip(nattunes, tunes)):
             raise ValueError(
                 "Driven tunes exist within the natural tune tolerance."
                 "Please check the input parameters."
