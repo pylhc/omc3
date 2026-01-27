d6e88b37b11e2028cce319791a03d519e1228340
Felix Soubelet <felix.soubelet@protonmail.com>
2026-01-27 17:50:38 +0100

    use S constant

--- omc3/model/model_creators/lhc_model_creator.py
+++ omc3/model/model_creators/lhc_model_creator.py
@@ -48,7 +48,7 @@ from omc3.model.model_creators.abstract_model_creator import (
     SegmentCreator,
     check_folder_choices,
 )
-from omc3.optics_measurements.constants import NAME
+from omc3.optics_measurements.constants import NAME, S
 from omc3.utils.iotools import create_dirs, get_check_by_regex_func, get_check_suffix_func
 
 if TYPE_CHECKING:
@@ -60,7 +60,7 @@ LOGGER = logging.getLogger(__name__)
 
 
 def _b2_columns() -> list[str]:
-    cols_outer = [f"{KP}{num}{S}L" for KP in ("K", "P") for num in range(21) for S in ("", "S")]
+    cols_outer = [f"{KP}{num}{S}L" for KP in ("K", "P") for num in range(21) for S in ("", S)]
     cols_middle = [
         "DX",
         "DY",
--- omc3/optics_measurements/chromatic.py
+++ omc3/optics_measurements/chromatic.py
@@ -10,7 +10,7 @@ from __future__ import annotations
 import numpy as np
 import pandas as pd
 
-from omc3.optics_measurements.constants import DELTA, ERR, MDL, S
+from omc3.optics_measurements.constants import DELTA, ERR, MDL, PHASE_ADV, S
 from omc3.optics_measurements.toolbox import df_prod, df_ratio
 
 
@@ -46,13 +46,13 @@ def calculate_w_and_phi(betas, dpps, input_files, measure_input, plane):
     joined[f"{ERR}PHI{plane}"] = 1 / (1 + np.square(a / b)) * np.sqrt(
         np.square(aerr / b) + np.square(berr * a / np.square(b))) / (2 * np.pi)
     output_df = pd.merge(measure_input.accelerator.model.loc[:,
-                         ["S", f"MU{plane}", f"BET{plane}", f"ALF{plane}", f"W{plane}",
+                         [S, f"{PHASE_ADV}{plane}", f"BET{plane}", f"ALF{plane}", f"W{plane}",
                           f"PHI{plane}"]],
                          joined.loc[:,
                          [f"{pref}{col}{plane}" for pref in ("", ERR) for col in ("W", "PHI")]],
                          how="inner", left_index=True,
                          right_index=True, suffixes=(MDL, ''))
-    output_df.rename(columns={"SMDL": "S"}, inplace=True)
+    output_df.rename(columns={"SMDL": S}, inplace=True)
     return output_df
 
 
