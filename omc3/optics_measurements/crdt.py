1a0d621a3ab53ac87984941f72f2b2c686d08d5b
Felix Soubelet <felix.soubelet@protonmail.com>
2026-01-30 22:32:53 +0100

    full link

--- omc3/optics_measurements/crdt.py
+++ omc3/optics_measurements/crdt.py
@@ -5,7 +5,7 @@ Combined RDTs
 This module contains combined resonance driving terms calculations functionality of
 ``optics_measurements``.
 It provides functions to compute combined resonance driving terms following the derivations in
-https://arxiv.org/pdf/1402.1461.
+https://arxiv.org/pdf/1402.1461.pdf.
 """
 
 from __future__ import annotations
