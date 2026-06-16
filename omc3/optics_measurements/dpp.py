42d3e1a7f8d0c91f95fbfa82853800251f3508dd
Felix Soubelet <felix.soubelet@protonmail.com>
2026-06-16 10:00:18 +0200

    quick formatting

--- omc3/optics_measurements/dpp.py
+++ omc3/optics_measurements/dpp.py
@@ -5,6 +5,7 @@ Dpp
 This module contains deltap over p calculations related functionality of ``optics_measurements``.
 It provides functions to computes and arrange dp over p.
 """
+
 from __future__ import annotations
 
 import logging
@@ -36,8 +37,10 @@ def arrange_dpps(dpps: Sequence[float], tolerance: float = DPP_BIN_TOLERANCE) ->
     zero_offset = np.mean(_values_in_range(_find_range_with_element(ranges, closest_to_zero), dpps))
     LOGGER.debug(f"dp/p closest to zero is {dpps[closest_to_zero]}")
     if np.abs(zero_offset) > tolerance:
-        LOGGER.warning(f"Analysed files have large momentum deviation {zero_offset}. "
-                       f"Optics parameters might be wrong.")
+        LOGGER.warning(
+            f"Analysed files have large momentum deviation {zero_offset}. "
+            "Optics parameters might be wrong."
+        )
     LOGGER.debug(f"Detected dpp differences, aranging as: {ranges}, zero offset: {zero_offset}.")
     arranged_dpps = []
     for idx in range(len(dpps)):
@@ -55,12 +58,14 @@ def _find_range_with_element(ranges: list[list[int]], element: int) -> list[int]
 
 
 def _compute_ranges(dpps: Sequence[float], tolerance: float) -> list[list[int]]:
-    """ Groups the indices of dpps in bins of tolerance.
-    The function first sorts the indices by their dpp value
-    adds a new group whenever the dpp of the next index is larger than the first value in the group.
+    """
+    Groups the indices of dpps in bins of tolerance.
+    The function first sorts the indices by their dpp value,
+    adds a new group whenever the dpp of the next index is larger
+    than the first value in the group.
 
-    Works for now, but we could improve by using the mean dpp of the group to check against,
-    i.e. `abs(np.mean(dpps[ranges[-1]]) - dpps[idx]) < tolerance`.
+    Works for now, but we could improve by using the mean dpp of the group to
+    check against, i.e. `abs(np.mean(dpps[ranges[-1]]) - dpps[idx]) < tolerance`.
     Only neccessary if we have some weird outliers. (jdilly)
     """
     ordered_ids = np.argsort(dpps)
@@ -75,18 +80,24 @@ def _compute_ranges(dpps: Sequence[float], tolerance: float) -> list[list[int]]:
     return ranges
 
 
-def append_amp_dpp(list_of_tfs: Sequence[tfs.TfsDataFrame], dpp_values: Sequence[float]) -> Sequence[tfs.TfsDataFrame]:
+def append_amp_dpp(
+    list_of_tfs: Sequence[tfs.TfsDataFrame], dpp_values: Sequence[float]
+) -> Sequence[tfs.TfsDataFrame]:
     """
     Add the dpp values to the DPP-header of the tfs files, if larger than the DPP-tolerance,
     otherwise set to zero. This is intended to the DPP value for on-momentum files to zero.
     """
     for i, dpp in enumerate(dpp_values):
-        list_of_tfs[i].headers["DPPAMP"] = dpp if (not np.isnan(dpp) and np.abs(dpp) > DPP_TOLERANCE) else 0.0
+        list_of_tfs[i].headers["DPPAMP"] = (
+            dpp if (not np.isnan(dpp) and np.abs(dpp) > DPP_TOLERANCE) else 0.0
+        )
     return list_of_tfs
 
 
-def append_dpp(list_of_tfs: Sequence[tfs.TfsDataFrame], dpp_values: Sequence[float]) -> Sequence[tfs.TfsDataFrame]:
-    """ Add the dpp values to the DPP-header of the tfs dataframes."""
+def append_dpp(
+    list_of_tfs: Sequence[tfs.TfsDataFrame], dpp_values: Sequence[float]
+) -> Sequence[tfs.TfsDataFrame]:
+    """Add the dpp values to the DPP-header of the tfs dataframes."""
     for i, dpp in enumerate(dpp_values):
         list_of_tfs[i].headers["DPP"] = dpp
     return list_of_tfs
@@ -103,14 +114,19 @@ def calculate_dpoverp(input_files: InputFiles, meas_input: DotDict) -> float | n
     Returns:
         Scalar dp/p for a single file, or an array of dp/p values per file.
     """
-    df_orbit = pd.DataFrame(meas_input.accelerator.model).loc[:, ['S', 'DX']]
-    df_orbit = pd.merge(df_orbit, input_files.joined_frame('X', ['CO', 'CORMS']), how='inner',
-                        left_index=True, right_index=True)
+    df_orbit = pd.DataFrame(meas_input.accelerator.model).loc[:, ["S", "DX"]]
+    df_orbit = pd.merge(
+        df_orbit,
+        input_files.joined_frame("X", ["CO", "CORMS"]),
+        how="inner",
+        left_index=True,
+        right_index=True,
+    )
     mask = meas_input.accelerator.get_element_types_mask(df_orbit.index, ["arc_bpm"])
     df_filtered = df_orbit.loc[mask, :]
     dispersions = df_filtered.loc[:, "DX"].to_numpy()
-    denom = np.sum(dispersions ** 2)
-    if denom == 0.:
+    denom = np.sum(dispersions**2)
+    if denom == 0.0:
         raise ValueError("Cannot compute dpp probably no arc BPMs.")
     amps = input_files.get_data(df_filtered, "CO")
     if amps.ndim == 1:
@@ -131,19 +147,24 @@ def calculate_amp_dpoverp(input_files: InputFiles, meas_input: DotDict) -> float
     Returns:
         Scalar dp/p amplitude for a single file, or an array of values per file.
     """
-    df_orbit = pd.DataFrame(meas_input.accelerator.model).loc[:, ['S', 'DX']]
-    df_orbit = pd.merge(df_orbit, input_files.joined_frame('X', ['AMPX', 'AMPZ']), how='inner',
-                        left_index=True, right_index=True)
+    df_orbit = pd.DataFrame(meas_input.accelerator.model).loc[:, ["S", "DX"]]
+    df_orbit = pd.merge(
+        df_orbit,
+        input_files.joined_frame("X", ["AMPX", "AMPZ"]),
+        how="inner",
+        left_index=True,
+        right_index=True,
+    )
     # TODO often missing AMPZ causes FutureWarning in future will be KeyError
     if np.all(np.isnan(input_files.get_data(df_orbit, "AMPZ"))):
         return np.zeros(len(input_files["X"]))
     mask = meas_input.accelerator.get_element_types_mask(df_orbit.index, ["arc_bpm"])
     df_filtered = df_orbit.loc[mask, :]
-    amps = input_files.get_data(df_filtered, 'AMPX') * input_files.get_data(df_filtered, 'AMPZ')
+    amps = input_files.get_data(df_filtered, "AMPX") * input_files.get_data(df_filtered, "AMPZ")
     mask_zeros = (amps > 0) if amps.ndim == 1 else (np.sum(amps, axis=1) > 0)
     dispersions = df_filtered.loc[mask_zeros, "DX"].to_numpy()
-    denom = np.sum(dispersions ** 2)
-    if denom == 0.:
+    denom = np.sum(dispersions**2)
+    if denom == 0.0:
         raise ValueError("Cannot compute dpp probably no arc BPMs.")
     if amps.ndim == 1:
         numer = np.sum(dispersions * amps[mask_zeros])
