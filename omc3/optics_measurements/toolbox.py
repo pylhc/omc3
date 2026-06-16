4dc8385b45ab9ca6f645a2f47cd363a24b9c3fb6
Felix Soubelet <felix.soubelet@protonmail.com>
2026-06-16 10:49:26 +0200

    full type hints and formatting

--- omc3/optics_measurements/toolbox.py
+++ omc3/optics_measurements/toolbox.py
@@ -5,73 +5,97 @@ Toolbox
 This module contains helper functionality for ``optics_measurements``.
 It provides functions to perform regularly used simple calculations.
 """
+
 from __future__ import annotations
+
+from typing import TYPE_CHECKING
 
 import numpy as np
 
+if TYPE_CHECKING:
+    import pandas as pd
 
-def df_diff(df, a_col, b_col):
-    """ Returns a column containing the difference between a_col and b_col """
+
+def df_diff(df: pd.DataFrame, a_col: str, b_col: str) -> np.ndarray:
+    """Returns a column containing the difference between a_col and b_col"""
     return df.loc[:, a_col].to_numpy() - df.loc[:, b_col].to_numpy()
 
 
-def df_sum(df, a_col, b_col):
-    """ Returns a column containing the sum of a_col and b_col """
+def df_sum(df: pd.DataFrame, a_col: str, b_col: str) -> np.ndarray:
+    """Returns a column containing the sum of a_col and b_col"""
     return df.loc[:, a_col].to_numpy() + df.loc[:, b_col].to_numpy()
 
 
-def df_ratio(df, a_col, b_col):
-    """ Returns a column containing the ratio between a_col and b_col """
+def df_ratio(df: pd.DataFrame, a_col: str, b_col: str) -> np.ndarray:
+    """Returns a column containing the ratio between a_col and b_col"""
     return df.loc[:, a_col].to_numpy() / df.loc[:, b_col].to_numpy()
 
 
-def df_prod(df, a_col, b_col):
-    """ Returns a column containing the product of a_col and b_col """
+def df_prod(df: pd.DataFrame, a_col: str, b_col: str) -> np.ndarray:
+    """Returns a column containing the product of a_col and b_col"""
     return df.loc[:, a_col].to_numpy() * df.loc[:, b_col].to_numpy()
 
 
-def df_rel_diff(df, a_col, b_col):
-    """ Returns a column containing the difference between a_col and b_col relative to b_col """
+def df_rel_diff(df: pd.DataFrame, a_col: str, b_col: str) -> np.ndarray:
+    """Returns a column containing the difference between a_col and b_col relative to b_col"""
     return df_ratio(df, a_col, b_col) - 1
 
 
 # with Errors ---
 
 
-def df_err_sum(df, a_err_col, b_err_col):
-    """ Returns a column containing the root of the sum-of-squares of a_err_col and b_err_col """
-    return np.sqrt(np.square(df.loc[:, a_err_col].to_numpy()) + np.square(df.loc[:, b_err_col].to_numpy()))
+def df_err_sum(df: pd.DataFrame, a_err_col: str, b_err_col: str) -> np.ndarray:
+    """Returns a column containing the root of the sum-of-squares of a_err_col and b_err_col"""
+    return np.sqrt(
+        np.square(df.loc[:, a_err_col].to_numpy()) + np.square(df.loc[:, b_err_col].to_numpy())
+    )
 
 
-def df_rel_err_sum(df, a_col, b_col, a_err_col, b_err_col):
-    """ Returns a column containing the root of the relative sum-of-square of a_col/a_err_col and b_col/b_err_col """
-    return np.sqrt(np.square(df_ratio(df, a_err_col, a_col)) + np.square(df_ratio(df, b_err_col, b_col)))
+def df_rel_err_sum(
+    df: pd.DataFrame, a_col: str, b_col: str, a_err_col: str, b_err_col: str
+) -> np.ndarray:
+    """Returns a column containing the root of the relative sum-of-square of a_col/a_err_col and b_col/b_err_col"""
+    return np.sqrt(
+        np.square(df_ratio(df, a_err_col, a_col)) + np.square(df_ratio(df, b_err_col, b_col))
+    )
 
 
-def df_sum_with_err(df, a_col, b_col, a_err_col, b_err_col):
-    """ Returns two columns containing the sum and the total errors of the given columns."""
+def df_sum_with_err(
+    df: pd.DataFrame, a_col: str, b_col: str, a_err_col: str, b_err_col: str
+) -> tuple[np.ndarray, np.ndarray]:
+    """Returns two columns containing the sum and the total errors of the given columns."""
     return df_sum(df, a_col, b_col), df_err_sum(df, a_err_col, b_err_col)
 
 
-def df_diff_with_err(df, a_col, b_col, a_err_col, b_err_col):
-    """ Returns two columns containing the difference and the total errors of the given columns."""
+def df_diff_with_err(
+    df: pd.DataFrame, a_col: str, b_col: str, a_err_col: str, b_err_col: str
+) -> tuple[np.ndarray, np.ndarray]:
+    """Returns two columns containing the difference and the total errors of the given columns."""
     return df_diff(df, a_col, b_col), df_err_sum(df, a_err_col, b_err_col)
 
 
-def df_rel_diff_with_err(df, a_col, b_col, a_err_col, b_err_col):
-    """ Returns two columns containing the relative difference and the total errors of the given columns."""
-    return (df_rel_diff(df, a_col, b_col),
-            np.abs(df_ratio(df, a_col, b_col)) * df_rel_err_sum(df, a_col, b_col, a_err_col, b_err_col))
+def df_rel_diff_with_err(
+    df: pd.DataFrame, a_col: str, b_col: str, a_err_col: str, b_err_col: str
+) -> tuple[np.ndarray, np.ndarray]:
+    """Returns two columns containing the relative difference and the total errors of the given columns."""
+    return (
+        df_rel_diff(df, a_col, b_col),
+        np.abs(df_ratio(df, a_col, b_col)) * df_rel_err_sum(df, a_col, b_col, a_err_col, b_err_col),
+    )
 
 
-def df_ratio_with_err(df, a_col, b_col, a_err_col, b_err_col):
-    """ Returns two columns containing the ratio and the total errors of the given columns."""
+def df_ratio_with_err(
+    df: pd.DataFrame, a_col: str, b_col: str, a_err_col: str, b_err_col: str
+) -> tuple[np.ndarray, np.ndarray]:
+    """Returns two columns containing the ratio and the total errors of the given columns."""
     ratio = df_ratio(df, a_col, b_col)
     return ratio, np.abs(ratio) * df_rel_err_sum(df, a_col, b_col, a_err_col, b_err_col)
 
 
-def df_prod_with_err(df, a_col, b_col, a_err_col, b_err_col):
-    """ Returns two columns containing the product and the total errors of the given columns."""
+def df_prod_with_err(
+    df: pd.DataFrame, a_col: str, b_col: str, a_err_col: str, b_err_col: str
+) -> tuple[np.ndarray, np.ndarray]:
+    """Returns two columns containing the product and the total errors of the given columns."""
     prod = df_prod(df, a_col, b_col)
     return prod, np.abs(prod) * df_rel_err_sum(df, a_col, b_col, a_err_col, b_err_col)
 
@@ -79,20 +103,21 @@ def df_prod_with_err(df, a_col, b_col, a_err_col, b_err_col):
 # Angular Calculations ---
 
 
-def df_ang_diff(df, a_col, b_col):
-    """ Returns a column containing the angular difference between angles a and b in [-0.5 , 0.5] """
+def df_ang_diff(df: pd.DataFrame, a_col: str, b_col: str) -> np.ndarray:
+    """Returns a column containing the angular difference between angles a and b in [-0.5 , 0.5]"""
     return ang_diff(df.loc[:, a_col].to_numpy(), df.loc[:, b_col].to_numpy())
 
 
-def ang_diff(a, b):
+def ang_diff(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+    """Returns the angular difference between angles a and b in [-0.5, 0.5]."""
     return ang_interval_check(ang_interval_check(a) - ang_interval_check(b))
 
 
-def ang_sum(a, b):
-    """ Returns a column containing the angular sum between angles a and b in [-0.5 , 0.5] """
+def ang_sum(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+    """Returns a column containing the angular sum between angles a and b in [-0.5 , 0.5]"""
     return ang_interval_check(ang_interval_check(a) + ang_interval_check(b))
 
 
-def ang_interval_check(ang):
-    """ Returns ang wrapped into [-0.5, 0.5] """
+def ang_interval_check(ang: np.ndarray) -> np.ndarray:
+    """Returns ang wrapped into [-0.5, 0.5]"""
     return np.where(np.abs(ang) > 0.5, ang - np.sign(ang), ang)
